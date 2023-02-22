ac5690e550b23a157fa8ae1fec00c687a403b606
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2023-02-22 00:12:09 -0500

    chore: Install Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./projects/playground-app/src/**/*.{html,ts}",
+    // "./projects/playground-lib/src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
